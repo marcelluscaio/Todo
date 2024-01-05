d2dee0e6afb96dab5e54bc05fe9f32911f60dd0f
Caio Cabral <ocaiocabral@gmail.com>
2024-01-05 09:36:21 -0500

    feat: extracting function from useeffect and passing generic api path

--- app/components/ContextProvider.tsx
+++ app/components/ContextProvider.tsx
@@ -10,12 +10,16 @@ import TodoList from "./TodoList";
 export const Context = createContext<ContextType | null>(null);
 
 export default function ContextProvider() {
-	useEffect(() => {
-		fetch("http://localhost:3000/api/getTasks", {
+	function getTasks() {
+		fetch("/api/getTasks", {
 			cache: "no-store",
 		})
 			.then((response) => response.json())
 			.then((data) => setToDo(data));
+	}
+
+	useEffect(() => {
+		getTasks();
 	}, []);
 
 	const [toDo, setToDo] = useState<ToDoItem[]>([]);
